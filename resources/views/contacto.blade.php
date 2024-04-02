8cb9fda905dc169a2204d7657f8c0f32e85d307d
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2024-04-02 00:56:28 -0600

    cambios a contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,33 +1,33 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+    <div class="relative h-80 w-full">
+        <h1 class="w-64 absolute z-20 top-8 left-3 text-2xl">Cada bocado es un paso hacia un futuro más saludable y sostenible</h1>
+        <div
+            class="z-10 bg-[url('../../../icatali/public/img/decoraciones/nube-contacto-2.svg')] bg-no-repeat bg-contain w-72 h-72 absolute right-0">
         </div>
-    </div> --}}
-    <h1>Cada bocado es un paso hacia un futuro más saludable y sostenible</h1>
-    <div class="w-10/12 mx-auto">
+        <div class="z-10 bg-[url('../../../icatali/public/img/decoraciones/nube-contacto.svg')] bg-no-repeat bg-contain w-full h-72
+        flex justify-center items-center absolute">
+        </div>
+    </div>
+
+    <div class="w-8/12 mx-auto">
         <form class="flex flex-col" action="">
             <label class="bebas-400 font-bold mb-2" for="name">NOMBRE:</label>
-            <input class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
+            <input
+                class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
                 type="text" name="nombre">
             <label class="bebas-400 font-bold mb-2" for="">CORREO:</label>
-            <input class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
+            <input
+                class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
                 type="text" name="nombre">
             <label class="bebas-400 font-bold mb-2" for="">TALÉFONO</label>
-            <input class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
+            <input
+                class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
                 type="text" name="nombre">
             <label class="bebas-400 font-bold mb-2" for="">COMENTARIO</label>
-            <input class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
+            <textarea class="bg-gray-200 appearance-none border-2 border-gray-300 rounded-3xl w-full h-36 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 mb-2"
                 type="text" name="nombre">
-            <input type="submit">
+            </textarea>
+            <input class="bg-verde-icatali w-fit font-bold px-6 py-3 rounded-full mt-4" type="submit">
         </form>
     </div>
 </x-app-layout>
--- resources/views/preguntas_frecuentes.blade.php
+++ resources/views/preguntas_frecuentes.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <h1 class="bg-[url('../../../icatali/public/img/decoraciones/nube-preguntas.svg')] bg-no-repeat bg-cover h-36 flex justify-center items-center text-3xl mb-8">¿Como podemos ayudarte?</h1>
+    <h1 class="bg-[url('../../../icatali/public/img/decoraciones/nube-preguntas.svg')] bg-no-repeat bg-cover h-36 flex justify-center items-center text-3xl mb-8 bebas">¿Como podemos ayudarte?</h1>
     <div x-data="{ open: false }" class="relative" x-on:click.outside="open = false">
         <!-- Botón del dropdown -->
         <div @click="open = !open" class="flex justify-center items-center bg-verde-icatali p-2 border-2">
