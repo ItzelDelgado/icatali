e20c9ee4263dda1ca9821b11290c4550c6ceb48c
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2024-04-02 22:05:36 -0600

    cambios a contacto

--- resources/views/sobre_nosotros.blade.php
+++ resources/views/sobre_nosotros.blade.php
@@ -42,30 +42,25 @@
     <div class="hidden md:block">
         <div class="flex justify-center">
             <div class="relative h-[27rem] mb-12">
-                <div class="relative right-14 bg-[#DFEEF4] rounded-full w-[25rem] h-[25rem]"></div>
-                <div class="absolute top-11">
+                <div class="relative  bg-[#DFEEF4] rounded-full w-[25rem] h-[25rem]"></div>
+                <div class="absolute top-11 right-10">
                     <h1 class="bebas text-3xl text-center mb-2">ACERCA DE ICATALI</h1>
-                    <p class="w-80 pl-3 text-center">Aunque cada persona es diferente, nosotros aseguramos que los
+                    <p class="w-80 text-center">Aunque cada persona es diferente, nosotros aseguramos que los
                         beneficios siempre estarán presentes gracias a que nos hemos adaptado a los nuevos estilos de
-                        vida o tipos
-                        de
-                        dieta
-                        como la restrictiva (por enfermedad), plant based, Veganos, Vegetarianos, Flexivegetariano,
-                        plant
-                        forward, entre otros, ofreciendo una diversidad de productos que pueden cubrir las necesidades
-                        de
-                        cada
-                        población.</p>
+                        vida o tipos de dieta como la restrictiva (por enfermedad), plant based, Veganos, Vegetarianos,
+                        Flexivegetariano, plant forward, entre otros, ofreciendo una diversidad de productos que pueden
+                        cubrir las necesidades
+                        de cada población.</p>
                     <button class="absolute left-48 -bottom-14 bg-black rounded-full text-white p-3 font-bold"><a
                             href="">CONTACTANOS</a></button>
                 </div>
             </div>
-            <div class="relative">
-                <img src="{{ asset('img/decoraciones/sobre-nosotros-banner.png') }}" alt="">
+            <div class="relative max-w-[700px] ml-20">
+                <img class="w-full" src="{{ asset('img/decoraciones/sobre-nosotros-banner.png') }}" alt="">
             </div>
         </div>
         <div class="relative">
-            <div class="container">
+            <div class="container max-w-[800px]">
                 <h2 class="text-2xl bebas font-semibold uppercase mb-4">¿QUIÉNES SOMOS?</h2>
                 <p>En ICATALI ofrecemos productos que se caracterizan por poseer un alto contenido de proteína de origen
                     vegetal digerible, que se diseñaron un enfoque emocional, nutrimental y funcional con soporte
@@ -73,17 +68,22 @@
                     científico</p>
                 <h2 class="text-2xl bebas font-semibold uppercase my-4">Compromiso con la Nutrición Sostenible</h2>
                 <p>En ICATALI, estamos comprometidos con la nutrición sostenible. Nuestros productos se destacan por su
-                    alto
-                    contenido de proteínas de origen vegetal digeribles, un paso audaz hacia un futuro alimentario más
-                    consciente y equilibrado.</p>
+                    alto contenido de proteínas de origen vegetal digeribles, un paso audaz hacia un futuro alimentario
+                    más consciente y equilibrado.</p>
                 <h2 class="text-2xl bebas font-semibold uppercase my-4">Diseño Emocional, Nutricional y Funcional</h2>
                 <p>Cada producto ICATALI se ha diseñado con un enfoque integral. Más allá de nutrir tu cuerpo, nos
                     esforzamos por ofrecer experiencias alimenticias que despierten tus emociones. Desde el primer
-                    bocado,
-                    queremos que sientas la conexión entre el bienestar físico y emocional.</p>
+                    bocado, queremos que sientas la conexión entre el bienestar físico y emocional.</p>
+                <div class="max-w-[400px] mx-auto mt-16">
+                    <img class="w-full" src="{{ asset('img/sobre_nosotros-semillas.png') }}" alt="">
+                </div>
             </div>
-            <div class="bg-[url('../../../icatali/public/img/decoraciones/nosotros-lateral-verde.svg')] bg-no-repeat bg-contain h-20 w-24 absolute"></div>
-            <div class="bg-[url('../../../icatali/public/img/decoraciones/nosotros-lateral-azul.svg')] bg-no-repeat bg-contain h-56 w-24 absolute right-0"></div>
+            <div
+                class="bg-[url('../../../icatali/public/img/decoraciones/nosotros-lateral-verde.svg')] bg-no-repeat bg-contain h-20 w-24 absolute">
+            </div>
+            <div
+                class="bg-[url('../../../icatali/public/img/decoraciones/nosotros-lateral-azul.svg')] bg-no-repeat bg-contain h-[23rem] w-[20rem] absolute right-0 top-52 ">
+            </div>
         </div>
     </div>
 </x-app-layout>
